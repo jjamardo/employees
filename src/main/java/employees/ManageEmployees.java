85ba72d4e3f11b4e0fca2e994c798a334c0a21de
Julian Jamardo <jamardo.julian@gmail.com>
2017-08-18 15:42:45 -0300

    Adding criteria query for join

--- src/main/java/employees/ManageEmployees.java
+++ src/main/java/employees/ManageEmployees.java
@@ -3,11 +3,17 @@ package employees;
 import java.util.List;
 import java.util.Date;
 import java.util.Iterator;
+
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.criterion.CriteriaSpecification;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.SessionFactory;
 
 public class ManageEmployees {
@@ -37,7 +43,44 @@ public class ManageEmployees {
 
 		//ME.listDepartments();
 
-		ME.listDeptEmp();
+		//ME.listDeptEmp();
+		
+		ME.Query();
+	}
+
+	public void Query() {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			
+			Criteria c = session.createCriteria(Employee.class, "employee");
+			c.createAlias("employee.deptEmps", "deptEmps");
+			c.createAlias("deptEmps.department", "department");
+			c.add(Restrictions.eq("department.deptName", "Sales"));
+
+			ProjectionList proList = Projections.projectionList();
+			proList.add(Projections.property("employee.firstName"));
+			proList.add(Projections.property("employee.lastName"));
+			proList.add(Projections.property("department.deptName"));
+			c.setProjection(proList);
+			
+			List<?> results = c.list();
+			for (Iterator<?> iterator = results.iterator(); iterator.hasNext();) {
+				Object[] result = (Object[]) iterator.next();
+				String firstName = (String) result[0];
+				String lastName = (String) result[1];
+				String deptName = (String) result[2];
+				System.out.println("Name: " + firstName + " " + lastName + " DeptName: " + deptName);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 	}
 
 	public void listDepartments() {
